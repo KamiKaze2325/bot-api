aeebfd31b0cf9bee7fb97dd65c3925e883f1dfb5
Kasjful Kurniawan <kasful.bpp@gmail.com>
2021-10-22 17:02:13 +0800

    add new

--- src/utils/convert-base64.js
+++ src/utils/convert-base64.js
@@ -1,6 +1,7 @@
 import fs from 'fs/promises';
 
 const convertBase64 = async (base64) => {
+    console.log(base64);
     const dateTime = new Date();
     const fileName = `${dateTime.getTime()}.webp`;
     const binaryData = new Buffer(base64, 'base64').toString('binary');
