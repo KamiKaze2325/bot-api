b42e43a710a8b26a3b9a9e4f888d67c8d28612ae
Kasjful Kurniawan <kasful.bpp@gmail.com>
2021-10-22 16:52:22 +0800

    fix: webp

--- src/utils/convert-base64.js
+++ src/utils/convert-base64.js
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 
 const convertBase64 = async (base64) => {
     const dateTime = new Date();
-    const fileName = `${dateTime.getTime()}.png`;
+    const fileName = `${dateTime.getTime()}.webp`;
     const binaryData = new Buffer(base64, 'base64').toString('binary');
     await fs.writeFile(`./files/${fileName}`, binaryData, "binary");
     return {
