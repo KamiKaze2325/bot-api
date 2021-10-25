25879aeb46dbd469fc99705947947ec6033eae2f
Kasjful Kurniawan <kasful.bpp@gmail.com>
2021-10-25 19:17:50 +0800

    change to 5 minutes

--- src/controllers/controller-bot-wa.js
+++ src/controllers/controller-bot-wa.js
@@ -39,7 +39,7 @@ const downloadFile = (req, res) => {
                       if (err) throw err;
                   });
               }
-          ,120000)
+          ,(5*60)*1000)
       } catch (error) {
           console.error(error)
       }
